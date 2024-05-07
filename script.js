3aca9a37e68258df5bf6ff2cb67abfb86b427ab6
riverportals <160665507+riverportals@users.noreply.github.com>
2024-05-07 21:22:13 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function showStars() {
+    Swal.fire({
+        imageUrl: "Images/Home/stars.png",
+        imageWidth: "100%",
+        background: "none",
+        showConfirmButton: false
+    });
+
+}
